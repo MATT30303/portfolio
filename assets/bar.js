ead80b5a2832eb7b1704021a5ad76b8551f281fe
Matt <fabianbarzola2003@gmail.com>
2024-01-03 09:15:59 -0300

    update
    new animations effects and styles

--- assets/bar.js
+++ assets/bar.js
@@ -1,4 +1,5 @@
 const toggle = document.getElementById("toggleBtn");
+const arrow = document.querySelector("#arrow");
 
 toggle.addEventListener("click", (e) => {
   document.getElementById("bar").classList.toggle("active");
