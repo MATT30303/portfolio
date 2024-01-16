7681d2581a588b4fd5d12346040bd3012714ab01
Matt <fabianbarzola2003@gmail.com>
2024-01-16 12:15:16 -0300

    add
    added new animation style to a card for info on about me section

--- assets/bar.js
+++ assets/bar.js
@@ -1,6 +1,14 @@
 const toggle = document.getElementById("toggleBtn");
 const arrow = document.querySelector("#arrow");
+const cardImg = document.getElementById("aboutMeImg");
+const card = document.getElementById("coverCard");
 
 toggle.addEventListener("click", (e) => {
   document.getElementById("bar").classList.toggle("active");
+});
+
+cardImg.addEventListener("click", (e) => {
+  console.log("que barbaro");
+  cardImg.classList.toggle("active");
+  card.classList.toggle("active");
 });
