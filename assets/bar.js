9c58d87131f082d5b80cd3ad4f0a948f9cd0d74c
Matt <fabianbarzola2003@gmail.com>
2024-01-24 16:46:50 -0300

    change
    change to the about me section

--- assets/bar.js
+++ assets/bar.js
@@ -1,7 +1,7 @@
 const toggle = document.getElementById("toggleBtn");
 const arrow = document.querySelector("#arrow");
 const cardImg = document.getElementById("aboutMeImg");
-const card = document.getElementById("coverCard");
+const card = document.getElementById("cardAboutMe");
 
 toggle.addEventListener("click", (e) => {
   document.getElementById("bar").classList.toggle("active");
